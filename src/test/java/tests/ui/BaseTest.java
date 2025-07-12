dfc1e70c1752a91819d3341dc78efaf3845c50b2
Aloshkya <alexis.belyakoff@yandex.ru>
2025-07-12 11:36:31 +0300

    Refactor init module

--- src/test/java/tests/ui/BaseTest.java
+++ src/test/java/tests/ui/BaseTest.java
@@ -84,7 +84,7 @@ public abstract class BaseTest {
         Configuration.baseUrl = "https://passport.kaiten.ru";
         Configuration.timeout = 30000;
         Configuration.clickViaJs = true;
-        Configuration.browserSize = null;
+        Configuration.browserSize = "1366x768";
 
         registrationPage = new RegistrationPage();
         loginPage = new LoginPage();
@@ -135,10 +135,10 @@ public abstract class BaseTest {
         options.addArguments("--disable-notifications");
         options.addArguments("--disable-popup-blocking");
         options.addArguments("--disable-infobars");
-//        options.addArguments("--headless");
-//        options.addArguments("--no-sandbox");
-//        options.addArguments("--disable-dev-shm-usage");
-//        options.addArguments("--disable-gpu");
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
         options.addArguments("--start-maximized");
         options.addArguments("--disable-blink-features=AutomationControlled");
         options.setExperimentalOption("excludeSwitches",
