db0acd9f3a3326e10d2fc7a816f7621bec02d2ac
Aloshkya <alexis.belyakoff@yandex.ru>
2025-07-12 17:54:05 +0300

    changed get messages logic

--- src/test/java/tests/ui/BaseTest.java
+++ src/test/java/tests/ui/BaseTest.java
@@ -76,7 +76,7 @@ public abstract class BaseTest {
         Configuration.baseUrl = "https://passport.kaiten.ru";
         Configuration.timeout = 30000;
         Configuration.clickViaJs = true;
-        Configuration.browserSize = null;
+        Configuration.browserSize = "1366x768";
 
         registrationPage = new RegistrationPage();
         loginPage = new LoginPage();
@@ -130,10 +130,10 @@ public abstract class BaseTest {
         options.addArguments("--disable-notifications");
         options.addArguments("--disable-popup-blocking");
         options.addArguments("--disable-infobars");
-//        options.addArguments("--headless");
-//        options.addArguments("--no-sandbox");
-//        options.addArguments("--disable-dev-shm-usage");
-//        options.addArguments("--disable-gpu");
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
         options.addArguments("--start-maximized");
         options.addArguments("--disable-blink-features=AutomationControlled");
         options.setExperimentalOption("excludeSwitches",
